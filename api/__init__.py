56d48a41149d733d03fb6018879347a78177a4ae
zzzzzwq <49017887+zzzzzwq@users.noreply.github.com>
2026-05-08 17:49:01 +0800

    auto sync 2026-05-08 17:49:01

--- api/__init__.py
+++ api/__init__.py
@@ -4,4 +4,4 @@ from flask import Blueprint
 api_bp = Blueprint("api", __name__, url_prefix="/api")
 
 
-from api import auth, holdings, analysis, notify
+from api import auth, holdings, analysis, notify, stock
